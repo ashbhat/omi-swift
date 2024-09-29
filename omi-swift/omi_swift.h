bf5619343be2d959323f15efe991719bf9c4e024
Ash Bhat <me@ashbhat.com>
2024-09-29 10:55:37 -0700

    Initial Commit

---
+++ omi-swift/omi_swift.h
@@ -0,0 +1,18 @@
+//
+//  omi_swift.h
+//  omi-swift
+//
+//  Created by Ash Bhat on 9/29/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for omi_swift.
+FOUNDATION_EXPORT double omi_swiftVersionNumber;
+
+//! Project version string for omi_swift.
+FOUNDATION_EXPORT const unsigned char omi_swiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <omi_swift/PublicHeader.h>
+
+
